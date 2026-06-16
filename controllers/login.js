d2d3b219cdf7e3bb4cf7d43244b28195c31b0a5f
Samir Bhandari <samirbhandari1231@gmail.com>
2026-06-16 17:52:01 +0545

    fix: sameSite none for cross-site cookie

--- controllers/login.js
+++ controllers/login.js
@@ -27,12 +27,12 @@ const loginController = async (req, res) => {
       { expiresIn: "10d" },
     );
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      // secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
-      maxAge: 10 * 24 * 60 * 60 * 1000,
-    });
+   res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,       
+  sameSite: "none",   
+  maxAge: 10 * 24 * 60 * 60 * 1000,
+});
 
     return res.status(200).json({ success: true, message: "Login Sucessfull", avatar: user.profile });
 
